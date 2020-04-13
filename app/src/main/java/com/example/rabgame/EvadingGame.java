3fb9e682640566c68405ade31f8e7e01b1fdf49c
Levchenko Dmitry <dmitrilevchenko23@gmail.com>
2020-04-13 23:49:09 +0300

    fixed the menu

---
+++ app/src/main/java/com/example/rabgame/EvadingGame.java
@@ -0,0 +1,14 @@
+package com.example.rabgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EvadingGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evading_game);
+    }
+}
--- app/src/main/java/com/example/rabgame/MainActivity.java
+++ app/src/main/java/com/example/rabgame/MainActivity.java
@@ -2,15 +2,58 @@ package com.example.rabgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button start,shop,rule,exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        start = (Button)findViewById(R.id.start);
+        shop = (Button)findViewById(R.id.shop);
+        rule = (Button)findViewById(R.id.rule);
+        exit = (Button)findViewById(R.id.exit);
+        start.setOnClickListener(CreatesetOnclickListener());
+        shop.setOnClickListener(CreatesetOnclickListener());
+        rule.setOnClickListener(CreatesetOnclickListener());
+        exit.setOnClickListener(CreatesetOnclickListener());
     }
+    public View.OnClickListener CreatesetOnclickListener()
+    {
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId())
+                {
+                    case R.id.start:
+                        CreateStartIntent(); break;
+                    case R.id.shop:
+                        CreateShopIntent(); break;
+                    case R.id.exit:
+                        CreateExitIntent();break;
+                }
+            }
+        };
+        return onClickListener;
+    }
+    void CreateStartIntent()
+    {
+        Intent intent = new Intent(MainActivity.this,ChooseTypeOfGame.class);
+        startActivity(intent);
+    }
+    void CreateExitIntent()
+    {
+        this.finish();
+    }
+    void CreateShopIntent()
+    {
+        Intent intent = new Intent(MainActivity.this,Shop.class);
+        startActivity(intent);
+    }
+
 }
